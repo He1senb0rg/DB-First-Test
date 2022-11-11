ee9c9a52868d7f13fb0793bc6d58133f05a15817
Heisenbørg <andresantosescola2020@gmail.com>
2022-11-11 21:21:52 +0000

    db first

---
+++ db_ado_ex/Models/Categoria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace db_ado_ex.Models
+{
+
+
+    /*
+     
+    CREATE TABLE [dbo].[Artigos] (
+      [Id]       INT           IDENTITY (1, 1) NOT NULL,
+      [Nome]     NVARCHAR (50) NOT NULL,
+      [Preco]    INT           NOT NULL,
+      [QtaStock] SMALLINT      NULL,
+      PRIMARY KEY CLUSTERED ([Id] ASC)
+    );
+
+    */
+
+
+    [Table("Categoria")]
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Nome { get; set; }
+    }
+}
---
+++ db_ado_ex/Models/ICategoriaDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace db_ado_ex.Models
+{
+    public interface ICategoriaDAL
+    {
+        IEnumerable<Categoria> GetAllCategorias();
+        void AddCategoria(Categoria categoria);
+        void UpdateCategoria(Categoria categoria);
+        Categoria GetCategoria(int? id);
+        void DeleteCategoria(int? id);
+    }
+}
